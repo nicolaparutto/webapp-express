2d73d676bff61c04fad42143b738229571ef293b
nicolaparutto <nicolaparutto2003@gmail.com>
2025-02-06 15:23:30 +0100

    feat: db connected & root index, show can return data from db

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,12 +1,27 @@
+const connection = require('../data/moviesDB');
+
 //Index (get)
 const index = (req, res) => {
-   res.send('Visualizzo la lista dei film')
+
+   const sql = 'SELECT * FROM movies';
+   connection.query(sql, (err, results) => {
+      if (err) return res.status(500).json({ error: 'Errore durante il caricamento dei film' });
+      res.json(results);
+   })
+
 }
 
 //Show (get)
 const show = (req, res) => {
    const id = req.params.id;
-   res.send(`Visualizzo il film con id ${id}`)
+
+   const sql = 'SELECT * FROM movies WHERE id = ?';
+   connection.query(sql, [id], (err, results) => {
+      if (err) return res.status(500).json({ error: 'Errore durante il caricamento del film' });
+      if (results.length === 0) return res.status(404).json({ error: 'Film non trovato' });
+      res.json(results[0]);
+   })
+
 }
 
 module.exports = {
