0666ebe10427526a3d7d0d0c797b72538c72ad89
nicolaparutto <nicolaparutto2003@gmail.com>
2025-02-06 15:48:38 +0100

    feat: add reviews to the call of a single film

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -16,10 +16,24 @@ const show = (req, res) => {
    const id = req.params.id;
 
    const sql = 'SELECT * FROM movies WHERE id = ?';
+   const sqlReviews = 'SELECT * FROM reviews WHERE reviews.movie_id = ?';
+   //movie query
    connection.query(sql, [id], (err, results) => {
+
       if (err) return res.status(500).json({ error: 'Errore durante il caricamento del film' });
       if (results.length === 0) return res.status(404).json({ error: 'Film non trovato' });
-      res.json(results[0]);
+
+      //reviews query
+      connection.query(sqlReviews, [id], (err, resultsReviews) => {
+         if (err) return res.status(500).json({ error: 'Errore durante il caricamento delle recensioni' });
+
+         const movie = results[0];
+         res.json({
+            ...movie,
+            reviews: resultsReviews
+         })
+      })
+
    })
 
 }
